a7b84bbd9d3c077bac1a275b975c00a8e991350c
Simon Schrape <s.schrape@epubli.com>
2016-05-25 13:41:06 +0200

    Extend PdfInfoOutputParser.

--- PdfInfoOutputParser.php
+++ PdfInfoOutputParser.php
@@ -21,19 +21,74 @@ class PdfInfoOutputParser
                 continue;
             }
 
-            $valueCleaned = str_replace(' ', '', $fields[1]);
+            $valueCleaned = trim($fields[1]);
 
-            if ($fields[0] == 'Pages') {
-                $result->setPagesTotal((int)$valueCleaned);
-            }
+            switch ($fields[0]) {
+                case 'Title':
+                    $result->setTitle($valueCleaned);
+                    break;
+                case 'Subject':
+                    $result->setSubject($valueCleaned);
+                    break;
+                case 'Keywords':
+                    $result->setKeywords($valueCleaned);
+                    break;
+                case 'Author':
+                    $result->setAuthor($valueCleaned);
+                    break;
+                case 'Creator':
+                    $result->setCreator($valueCleaned);
+                    break;
+                case 'Producer':
+                    $result->setProducer($valueCleaned);
+                    break;
+                case 'CreationDate':
+                    $result->setCreationDate(new \DateTime($valueCleaned));
+                    break;
+                case 'ModDate':
+                    $result->setModificationDate(new \DateTime($valueCleaned));
+                    break;
+                case 'Tagged':
+                    $result->setTagged($valueCleaned == 'yes');
+                    break;
+                case 'UserProperties':
+                    $result->setUserProperties($valueCleaned == 'yes');
+                    break;
+                case 'Suspects':
+                    $result->setSuspects($valueCleaned == 'yes');
+                    break;
+                case 'Form':
+                    $result->setForm($valueCleaned);
+                    break;
+                case 'JavaScript':
+                    $result->setJavaScript($valueCleaned == 'yes');
+                    break;
+                case 'Pages':
+                    $result->setPagesTotal((int)$valueCleaned);
+                    break;
+                case 'Encrypted':
+                    $result->setEncrypted($valueCleaned == 'yes');
+                    break;
+                case 'Page size':
+                    $valueCleaned = str_replace('pts', '', $valueCleaned);
+                    $valueCleaned = preg_replace('/(\(.*\))/', '', $valueCleaned);
 
-            if ($fields[0] == 'Page size') {
-                $valueCleaned = str_replace('pts', '', $valueCleaned);
-                $valueCleaned = preg_replace('/(\(.*\))/', '', $valueCleaned);
-
-                $valueArray = preg_split('/x/', $valueCleaned);
-                $result->setWidth((int)$valueArray[0]);
-                $result->setHeight((int)$valueArray[1]);
+                    $valueArray = preg_split('/x/', $valueCleaned);
+                    $result->setWidth((int)$valueArray[0]);
+                    $result->setHeight((int)$valueArray[1]);
+                    break;
+                case 'Page rot':
+                    $result->setRotation((int)$valueCleaned);
+                    break;
+                case 'File size':
+                    $result->setFileSize((int)$valueCleaned);
+                    break;
+                case 'Optimized':
+                    $result->setLinearized($valueCleaned == 'yes');
+                    break;
+                case 'PDF version':
+                    $result->setPdfVersion($valueCleaned);
+                    break;
             }
         }
 
--- PdfInfoParserResult.php
+++ PdfInfoParserResult.php
@@ -5,9 +5,84 @@ namespace Epubli\Pdf\XpdfTools;
 class PdfInfoParserResult
 {
     /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var string
+     */
+    private $subject;
+
+    /**
+     * @var string
+     */
+    private $keywords;
+
+    /**
+     * @var string
+     */
+    private $author;
+
+    /**
+     * @var string
+     */
+    private $creator;
+
+    /**
+     * @var string
+     */
+    private $producer;
+
+    /**
+     * @var \DateTime
+     */
+    private $creationDate;
+
+    /**
+     * @var \DateTime
+     */
+    private $modificationDate;
+
+    /**
+     * @var bool
+     */
+    private $tagged;
+
+    /**
+     * @var bool
+     */
+    private $userProperties;
+
+    /**
+     * @var bool
+     */
+    private $suspects;
+
+    /**
+     * @var string
+     */
+    private $form;
+
+    /**
+     * @var bool
+     */
+    private $javaScript;
+
+    /**
      * @var int
      */
     private $pagesTotal;
+
+    /**
+     * @var bool
+     */
+    private $encrypted;
+
+    /**
+     * @var string
+     */
+    private $permissions;
 
     /**
      * @var int
@@ -18,6 +93,234 @@ class PdfInfoParserResult
      * @var int
      */
     private $height;
+
+    /**
+     * @var int
+     */
+    private $rotation;
+
+    /**
+     * @var int
+     */
+    private $fileSize;
+
+    /**
+     * @var bool
+     */
+    private $linearized;
+
+    /**
+     * @var string
+     */
+    private $pdfVersion;
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * @param string $subject
+     */
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
+    }
+
+    /**
+     * @return string
+     */
+    public function getKeywords()
+    {
+        return $this->keywords;
+    }
+
+    /**
+     * @param string $keywords
+     */
+    public function setKeywords($keywords)
+    {
+        $this->keywords = $keywords;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param string $author
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCreator()
+    {
+        return $this->creator;
+    }
+
+    /**
+     * @param string $creator
+     */
+    public function setCreator($creator)
+    {
+        $this->creator = $creator;
+    }
+
+    /**
+     * @return string
+     */
+    public function getProducer()
+    {
+        return $this->producer;
+    }
+
+    /**
+     * @param string $producer
+     */
+    public function setProducer($producer)
+    {
+        $this->producer = $producer;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreationDate()
+    {
+        return $this->creationDate;
+    }
+
+    /**
+     * @param \DateTime $creationDate
+     */
+    public function setCreationDate($creationDate)
+    {
+        $this->creationDate = $creationDate;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getModificationDate()
+    {
+        return $this->modificationDate;
+    }
+
+    /**
+     * @param \DateTime $modificationDate
+     */
+    public function setModificationDate($modificationDate)
+    {
+        $this->modificationDate = $modificationDate;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isTagged()
+    {
+        return $this->tagged;
+    }
+
+    /**
+     * @param boolean $tagged
+     */
+    public function setTagged($tagged)
+    {
+        $this->tagged = $tagged;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function hasUserProperties()
+    {
+        return $this->userProperties;
+    }
+
+    /**
+     * @param boolean $userProperties
+     */
+    public function setUserProperties($userProperties)
+    {
+        $this->userProperties = $userProperties;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function hasSuspects()
+    {
+        return $this->suspects;
+    }
+
+    /**
+     * @param boolean $suspects
+     */
+    public function setSuspects($suspects)
+    {
+        $this->suspects = $suspects;
+    }
+
+    /**
+     * @return string
+     */
+    public function getForm()
+    {
+        return $this->form;
+    }
+
+    /**
+     * @param string $form
+     */
+    public function setForm($form)
+    {
+        $this->form = $form;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function hasJavaScript()
+    {
+        return $this->javaScript;
+    }
+
+    /**
+     * @param boolean $javaScript
+     */
+    public function setJavaScript($javaScript)
+    {
+        $this->javaScript = $javaScript;
+    }
 
     /**
      * @return int
@@ -33,6 +336,38 @@ class PdfInfoParserResult
     public function setPagesTotal($pagesTotal)
     {
         $this->pagesTotal = $pagesTotal;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isEncrypted()
+    {
+        return $this->encrypted;
+    }
+
+    /**
+     * @param boolean $encrypted
+     */
+    public function setEncrypted($encrypted)
+    {
+        $this->encrypted = $encrypted;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPermissions()
+    {
+        return $this->permissions;
+    }
+
+    /**
+     * @param string $permissions
+     */
+    public function setPermissions($permissions)
+    {
+        $this->permissions = $permissions;
     }
 
     /**
@@ -65,5 +400,69 @@ class PdfInfoParserResult
     public function setHeight($height)
     {
         $this->height = $height;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRotation()
+    {
+        return $this->rotation;
+    }
+
+    /**
+     * @param int $rotation
+     */
+    public function setRotation($rotation)
+    {
+        $this->rotation = $rotation;
+    }
+
+    /**
+     * @return int
+     */
+    public function getFileSize()
+    {
+        return $this->fileSize;
+    }
+
+    /**
+     * @param int $fileSize
+     */
+    public function setFileSize($fileSize)
+    {
+        $this->fileSize = $fileSize;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isLinearized()
+    {
+        return $this->linearized;
+    }
+
+    /**
+     * @param boolean $linearized
+     */
+    public function setLinearized($linearized)
+    {
+        $this->linearized = $linearized;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPdfVersion()
+    {
+        return $this->pdfVersion;
+    }
+
+    /**
+     * @param string $pdfVersion
+     */
+    public function setPdfVersion($pdfVersion)
+    {
+        $this->pdfVersion = $pdfVersion;
     }
 }
