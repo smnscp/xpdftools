c7703a9cb2bd421efab0530c0428d7217207f0ad
Simon Schrape <s.schrape@epubli.com>
2018-02-22 09:24:56 +0100

    Extract another ProcessBuilder base class to eliminate code dupe.

--- BaseProcessBuilder.php
+++ BaseProcessBuilder.php
@@ -2,119 +2,11 @@
 
 namespace Epubli\Pdf\XpdfTools;
 
-use Symfony\Component\Process\Process;
-use Symfony\Component\Process\ProcessBuilder;
-
-abstract class BaseProcessBuilder
+abstract class BaseProcessBuilder extends \Epubli\Process\BaseProcessBuilder
 {
     /**
-     * @var ProcessBuilder
-     */
-    private $builder;
-
-    /**
-     * @var string path to input PDF file
-     */
-    private $inputFile;
-
-    /**
-     * @var string path to output file
-     */
-    private $outputFile;
-
-    /**
-     * @param string $executable
-     */
-    public function __construct($executable)
-    {
-        $this->builder = new ProcessBuilder();
-        $this->builder->setPrefix($executable);
-    }
-
-    /**
-     * @return Process
-     */
-    public function getProcess()
-    {
-        if ($this->inputFile) {
-            $this->builder->add($this->inputFile);
-        }
-
-        if ($this->outputFile) {
-            if (!$this->inputFile) {
-                // there must be an input file specified before the output file
-                $this->builder->add('-');
-            }
-            $this->builder->add($this->outputFile);
-        }
-
-        return $this->builder->getProcess();
-    }
-
-    /**
-     * Set the path to the PDF file.
-     *
-     * @param string $path path to PDF file
-     * @return $this
-     */
-    public function setInputFile($path)
-    {
-        $this->inputFile = $path;
-
-        return $this;
-    }
-
-    /**
-     * Let Xpdf tools expect input from stdin.
-     *
-     * @return $this
-     */
-    public function setInputStdin()
-    {
-        return $this->setInputFile('-');
-    }
-
-    /**
-     * Let Xpdf tools read $input from stdin.
-     *
-     * @param string $input PDF byte string coming from stdin
-     * @return $this
-     */
-    public function setInput($input)
-    {
-        $this->builder->setInput($input);
-
-        return $this->setInputStdin();
-    }
-
-    /**
-     * Set the path to the output file.
-     * This can be a single file (pdftops, pdftotext) or a path prefix (pdfimages, pdftohtml, pdftoppm).
-     *
-     * @param string $path path to output file
-     * @return $this
-     */
-    public function setOutputFile($path)
-    {
-        $this->outputFile = $path;
-
-        return $this;
-    }
-
-    /**
-     * Set stdout as output.
-     *
-     * @return $this
-     */
-    public function setOutputStdout()
-    {
-        return $this->setOutputFile('-');
-    }
-
-    #region Options
-    /**
      * Set the first page to process
-     * 
+     *
      * @param int $num page number
      * @return $this
      */
@@ -125,7 +17,7 @@ abstract class BaseProcessBuilder
 
     /**
      * Set the last page to process
-     * 
+     *
      * @param int $num page number
      * @return $this
      */
@@ -166,7 +58,7 @@ abstract class BaseProcessBuilder
 
     /**
      * Set a command line option with name $option.
-     * 
+     *
      * @param string $option name of the command line option (w/o dash)
      * @return $this
      */
@@ -179,5 +71,4 @@ abstract class BaseProcessBuilder
         
         return $this;
     }
-    #endregion Options
 }
